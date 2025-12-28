5d80a20b2f3ddeca5eefd6ad114c12b3a0282e6d
Yonibelay233 <User@>
2025-12-28 12:45:49 +0200

    finshed api server

--- API Project/Controllers/InsertController.cs
+++ API Project/Controllers/InsertController.cs
@@ -5,7 +5,7 @@ using WingsOfKaramboProject;
 
 namespace API_Project.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class InsertController : ControllerBase
     {
@@ -19,6 +19,8 @@ namespace API_Project.Controllers
             return x;
         }
 
+        [HttpPost]
+        [ActionName("InsertAStaffMember")]
         public int InsertAStaffMember([FromBody] StaffMemberTBL staffMemberTBL)
         {
             StaffMemberDB db = new StaffMemberDB();
@@ -27,124 +29,154 @@ namespace API_Project.Controllers
             return x;
         }
 
-        //public int InsertASpecialNeeds([FromBody] SpecialNeedsTBL specialNeedsTBL)
-        //{
-        //    SpecialNeedsDB db = new SpecialNeedsDB();
-        //    db.Insert(specialNeedsTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertASpecialNeeds")]
+        public int InsertASpecialNeeds([FromBody] SpecialNeedsTBL specialNeedsTBL)
+        {
+            SpecialNeedsDB db = new SpecialNeedsDB();
+            db.Insert(specialNeedsTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAAssigningChildrenToAGroup([FromBody] AssigningChildrenToAGroupTBL assigningChildrenToAGroupTBL)
-        //{
-        //    AssigningChildrenToAGroupDB db = new AssigningChildrenToAGroupDB();
-        //    db.Insert(assigningChildrenToAGroupTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAAssigningChildrenToAGroup")]
+        public int InsertAAssigningChildrenToAGroup([FromBody] AssigningChildrenToAGroupTBL assigningChildrenToAGroupTBL)
+        {
+            AssigningChildrenToAGroupDB db = new AssigningChildrenToAGroupDB();
+            db.Insert(assigningChildrenToAGroupTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAAssigningApprenticeToAGroup([FromBody] AssigningApprenticeToAGroupTBL assigningApprenticeToAGroupTBL)
-        //{
-        //    AssigningApprenticeToAGroupDB db = new AssigningApprenticeToAGroupDB();
-        //    db.Insert(assigningApprenticeToAGroupTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAAssigningApprenticeToAGroup")]
+        public int InsertAAssigningApprenticeToAGroup([FromBody] AssigningApprenticeToAGroupTBL assigningApprenticeToAGroupTBL)
+        {
+            AssigningApprenticeToAGroupDB db = new AssigningApprenticeToAGroupDB();
+            db.Insert(assigningApprenticeToAGroupTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAAssigningGroupToAction([FromBody] AssigningGroupToActionTBL assigningGroupToActionTBL)
-        //{
-        //    AssigningGroupToActionDB db = new AssigningGroupToActionDB();
-        //    db.Insert(assigningGroupToActionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAAssigningGroupToAction")]
+        public int InsertAAssigningGroupToAction([FromBody] AssigningGroupToActionTBL assigningGroupToActionTBL)
+        {
+            AssigningGroupToActionDB db = new AssigningGroupToActionDB();
+            db.Insert(assigningGroupToActionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAAssigningApprenticeToAction([FromBody] AssigningApprenticeToActionTBL assigningApprenticeToActionTBL)
-        //{
-        //    AssigningApprenticeToActionDB db = new AssigningApprenticeToActionDB();
-        //    db.Insert(assigningApprenticeToActionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAAssigningApprenticeToAction")]
+        public int InsertAAssigningApprenticeToAction([FromBody] AssigningApprenticeToActionTBL assigningApprenticeToActionTBL)
+        {
+            AssigningApprenticeToActionDB db = new AssigningApprenticeToActionDB();
+            db.Insert(assigningApprenticeToActionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAChildWithSpecialNeed([FromBody] ChildWithSpecialNeedTBL childWithSpecialNeedTBL)
-        //{
-        //    ChildWithSpecialNeedDB db = new ChildWithSpecialNeedDB();
-        //    db.Insert(childWithSpecialNeedTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAChildWithSpecialNeed")]
+        public int InsertAChildWithSpecialNeed([FromBody] ChildWithSpecialNeedTBL childWithSpecialNeedTBL)
+        {
+            ChildWithSpecialNeedDB db = new ChildWithSpecialNeedDB();
+            db.Insert(childWithSpecialNeedTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAPerson([FromBody] PersonTBL personTBL)
-        //{
-        //    PersonDB db = new PersonDB();
-        //    db.Insert(personTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAPerson")]
+        public int InsertAPerson([FromBody] PersonTBL personTBL)
+        {
+            PersonDB db = new PersonDB();
+            db.Insert(personTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertARole([FromBody] RoleTBL roleTBL)
-        //{
-        //    RoleDB db = new RoleDB();
-        //    db.Insert(roleTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertARole")]
+        public int InsertARole([FromBody] RoleTBL roleTBL)
+        {
+            RoleDB db = new RoleDB();
+            db.Insert(roleTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAGuide([FromBody] GuideTBL guideTBL)
-        //{
-        //    GuideDB db = new GuideDB();
-        //    db.Insert(guideTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAGuide")]
+        public int InsertAGuide([FromBody] GuideTBL guideTBL)
+        {
+            GuideDB db = new GuideDB();
+            db.Insert(guideTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertASchool([FromBody] SchoolTBL schoolTBL)
-        //{
-        //    SchoolDB db = new SchoolDB();
-        //    db.Insert(schoolTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertASchool")]
+        public int InsertASchool([FromBody] SchoolTBL schoolTBL)
+        {
+            SchoolDB db = new SchoolDB();
+            db.Insert(schoolTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAGroups([FromBody] GroupsTBL groupsTBL)
-        //{
-        //    GroupsDB db = new GroupsDB();
-        //    db.Insert(groupsTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAGroups")]
+        public int InsertAGroups([FromBody] GroupsTBL groupsTBL)
+        {
+            GroupsDB db = new GroupsDB();
+            db.Insert(groupsTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAGrade([FromBody] GradeTBL gradeTBL)
-        //{
-        //    GradeDB db = new GradeDB();
-        //    db.Insert(gradeTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAGrade")]
+        public int InsertAGrade([FromBody] GradeTBL gradeTBL)
+        {
+            GradeDB db = new GradeDB();
+            db.Insert(gradeTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertABranch([FromBody] BranchTBL branchTBL)
-        //{
-        //    BranchDB db = new BranchDB();
-        //    db.Insert(branchTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertABranch")]
+        public int InsertABranch([FromBody] BranchTBL branchTBL)
+        {
+            BranchDB db = new BranchDB();
+            db.Insert(branchTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAApprentice([FromBody] ApprenticeTBL apprenticeTBL)
-        //{
-        //    ApprenticeDB db = new ApprenticeDB();
-        //    db.Insert(apprenticeTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAApprentice")]
+        public int InsertAApprentice([FromBody] ApprenticeTBL apprenticeTBL)
+        {
+            ApprenticeDB db = new ApprenticeDB();
+            db.Insert(apprenticeTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //public int InsertAAction([FromBody] ActionTBL actionTBL)
-        //{
-        //    ActionDB db = new ActionDB();
-        //    db.Insert(actionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        [HttpPost]
+        [ActionName("InsertAAction")]
+        public int InsertAAction([FromBody] ActionTBL actionTBL)
+        {
+            ActionDB db = new ActionDB();
+            db.Insert(actionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
     }
 }
--- API Project/Controllers/UpdateController.cs
+++ API Project/Controllers/UpdateController.cs
@@ -5,195 +5,195 @@ using WingsOfKaramboProject;
 
 namespace API_Project.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class UpdateController : ControllerBase
     {
-        //[HttpPut]
-        //[ActionName("CityTBLUpdater")]
+        [HttpPut]
+        [ActionName("CityTBLUpdater")]
 
-        //public int UpdateACityTBL(CityTBL cityTBL)
-        //{
-        //    CityDB db = new CityDB();
-        //    db.Update(cityTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateACityTBL(CityTBL cityTBL)
+        {
+            CityDB db = new CityDB();
+            db.Update(cityTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("ActionTBLUpdater")]
+        [HttpPut]
+        [ActionName("ActionTBLUpdater")]
 
-        //public int UpdateAActionTBL(ActionTBL actionTBL)
-        //{
-        //    ActionDB db = new ActionDB();
-        //    db.Update(actionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAActionTBL(ActionTBL actionTBL)
+        {
+            ActionDB db = new ActionDB();
+            db.Update(actionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("ApprenticeTBLUpdater")]
+        [HttpPut]
+        [ActionName("ApprenticeTBLUpdater")]
 
-        //public int UpdateAApprenticeTBL(ApprenticeTBL apprenticeTBL)
-        //{
-        //    ApprenticeDB db = new ApprenticeDB();
-        //    db.Update(apprenticeTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAApprenticeTBL(ApprenticeTBL apprenticeTBL)
+        {
+            ApprenticeDB db = new ApprenticeDB();
+            db.Update(apprenticeTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("BranchTBLUpdater")]
+        [HttpPut]
+        [ActionName("BranchTBLUpdater")]
 
-        //public int UpdateABranchTBL(BranchTBL branchTBL)
-        //{
-        //    BranchDB db = new BranchDB();
-        //    db.Update(branchTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateABranchTBL(BranchTBL branchTBL)
+        {
+            BranchDB db = new BranchDB();
+            db.Update(branchTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("GradeTBLUpdater")]
+        [HttpPut]
+        [ActionName("GradeTBLUpdater")]
 
-        //public int UpdateAGradeTBL(GradeTBL gradeTBL)
-        //{
-        //    GradeDB db = new GradeDB();
-        //    db.Update(gradeTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAGradeTBL(GradeTBL gradeTBL)
+        {
+            GradeDB db = new GradeDB();
+            db.Update(gradeTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("GroupsTBLUpdater")]
+        [HttpPut]
+        [ActionName("GroupsTBLUpdater")]
 
-        //public int UpdateAGroupsTBL(GroupsTBL groupsTBL)
-        //{
-        //    GroupsDB db = new GroupsDB();
-        //    db.Update(groupsTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAGroupsTBL(GroupsTBL groupsTBL)
+        {
+            GroupsDB db = new GroupsDB();
+            db.Update(groupsTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("SchoolTBLUpdater")]
+        [HttpPut]
+        [ActionName("SchoolTBLUpdater")]
 
-        //public int UpdateASchoolTBL(SchoolTBL schoolTBL)
-        //{
-        //    SchoolDB db = new SchoolDB();
-        //    db.Update(schoolTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateASchoolTBL(SchoolTBL schoolTBL)
+        {
+            SchoolDB db = new SchoolDB();
+            db.Update(schoolTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("GuideTBLUpdater")]
+        [HttpPut]
+        [ActionName("GuideTBLUpdater")]
 
-        //public int UpdateAGuideTBL(GuideTBL guideTBL)
-        //{
-        //    GuideDB db = new GuideDB();
-        //    db.Update(guideTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAGuideTBL(GuideTBL guideTBL)
+        {
+            GuideDB db = new GuideDB();
+            db.Update(guideTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("RoleTBLUpdater")]
+        [HttpPut]
+        [ActionName("RoleTBLUpdater")]
 
-        //public int UpdateARoleTBL(RoleTBL roleTBL)
-        //{
-        //    RoleDB db = new RoleDB();
-        //    db.Update(roleTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateARoleTBL(RoleTBL roleTBL)
+        {
+            RoleDB db = new RoleDB();
+            db.Update(roleTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("PersonTBLUpdater")]
+        [HttpPut]
+        [ActionName("PersonTBLUpdater")]
 
-        //public int UpdateAPersonTBL(PersonTBL personTBL)
-        //{
-        //    PersonDB db = new PersonDB();
-        //    db.Update(personTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAPersonTBL(PersonTBL personTBL)
+        {
+            PersonDB db = new PersonDB();
+            db.Update(personTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("ChildWithSpecialNeedTBLUpdater")]
+        [HttpPut]
+        [ActionName("ChildWithSpecialNeedTBLUpdater")]
 
-        //public int UpdateAChildWithSpecialNeedTBL(ChildWithSpecialNeedTBL childWithSpecialNeedTBL)
-        //{
-        //    ChildWithSpecialNeedDB db = new ChildWithSpecialNeedDB();
-        //    db.Update(childWithSpecialNeedTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAChildWithSpecialNeedTBL(ChildWithSpecialNeedTBL childWithSpecialNeedTBL)
+        {
+            ChildWithSpecialNeedDB db = new ChildWithSpecialNeedDB();
+            db.Update(childWithSpecialNeedTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("StaffMemberTBLUpdater")]
+        [HttpPut]
+        [ActionName("StaffMemberTBLUpdater")]
 
-        //public int UpdateAStaffMemberTBL(StaffMemberTBL staffMemberTBL)
-        //{
-        //    StaffMemberDB db = new StaffMemberDB();
-        //    db.Update(staffMemberTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAStaffMemberTBL(StaffMemberTBL staffMemberTBL)
+        {
+            StaffMemberDB db = new StaffMemberDB();
+            db.Update(staffMemberTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("SpecialNeedsTBLUpdater")]
+        [HttpPut]
+        [ActionName("SpecialNeedsTBLUpdater")]
 
-        //public int UpdateASpecialNeedsTBL(SpecialNeedsTBL specialNeedsTBL)
-        //{
-        //    SpecialNeedsDB db = new SpecialNeedsDB();
-        //    db.Update(specialNeedsTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateASpecialNeedsTBL(SpecialNeedsTBL specialNeedsTBL)
+        {
+            SpecialNeedsDB db = new SpecialNeedsDB();
+            db.Update(specialNeedsTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("AssigningChildrenToAGroupTBLUpdater")]
+        [HttpPut]
+        [ActionName("AssigningChildrenToAGroupTBLUpdater")]
 
-        //public int UpdateAAssigningChildrenToAGroupTBL(AssigningChildrenToAGroupTBL assigningChildrenToAGroupTBL)
-        //{
-        //    AssigningChildrenToAGroupDB db = new AssigningChildrenToAGroupDB();
-        //    db.Update(assigningChildrenToAGroupTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAAssigningChildrenToAGroupTBL(AssigningChildrenToAGroupTBL assigningChildrenToAGroupTBL)
+        {
+            AssigningChildrenToAGroupDB db = new AssigningChildrenToAGroupDB();
+            db.Update(assigningChildrenToAGroupTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("AssigningApprenticeToAGroupTBLUpdater")]
+        [HttpPut]
+        [ActionName("AssigningApprenticeToAGroupTBLUpdater")]
 
-        //public int UpdateAAssigningApprenticeToAGroupTBL(AssigningApprenticeToAGroupTBL assigningApprenticeToAGroupTBL)
-        //{
-        //    AssigningApprenticeToAGroupDB db = new AssigningApprenticeToAGroupDB();
-        //    db.Update(assigningApprenticeToAGroupTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAAssigningApprenticeToAGroupTBL(AssigningApprenticeToAGroupTBL assigningApprenticeToAGroupTBL)
+        {
+            AssigningApprenticeToAGroupDB db = new AssigningApprenticeToAGroupDB();
+            db.Update(assigningApprenticeToAGroupTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("AssigningGroupToActionTBLUpdater")]
+        [HttpPut]
+        [ActionName("AssigningGroupToActionTBLUpdater")]
 
-        //public int UpdateAAssigningGroupToActionTBL(AssigningGroupToActionTBL assigningGroupToActionTBL)
-        //{
-        //    AssigningGroupToActionDB db = new AssigningGroupToActionDB();
-        //    db.Update(assigningGroupToActionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAAssigningGroupToActionTBL(AssigningGroupToActionTBL assigningGroupToActionTBL)
+        {
+            AssigningGroupToActionDB db = new AssigningGroupToActionDB();
+            db.Update(assigningGroupToActionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
 
-        //[HttpPut]
-        //[ActionName("AssigningApprenticeToActionTBLUpdater")]
+        [HttpPut]
+        [ActionName("AssigningApprenticeToActionTBLUpdater")]
 
-        //public int UpdateAAssigningApprenticeToActionTBL(AssigningApprenticeToActionTBL assigningApprenticeToActionTBL)
-        //{
-        //    AssigningApprenticeToActionDB db = new AssigningApprenticeToActionDB();
-        //    db.Update(assigningApprenticeToActionTBL);
-        //    int x = db.SaveChanges();
-        //    return x;
-        //}
+        public int UpdateAAssigningApprenticeToActionTBL(AssigningApprenticeToActionTBL assigningApprenticeToActionTBL)
+        {
+            AssigningApprenticeToActionDB db = new AssigningApprenticeToActionDB();
+            db.Update(assigningApprenticeToActionTBL);
+            int x = db.SaveChanges();
+            return x;
+        }
     }
 }
